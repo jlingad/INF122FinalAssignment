baada5ff2ff1a89beeec04e751ad32e4483f34e3
Cameron Watt <cawatt@uci.edu>
2016-03-08 15:01:50 -0800

    Added functionality to the PlayableFactory, as well as constructors that initialize a logic object and state object of their respective types

--- src/server/TicTacToeEngine.java
+++ src/server/TicTacToeEngine.java
@@ -1,7 +1,14 @@
 package server;
 
 public class TicTacToeEngine implements Playable {
-
+	TicTacToeLogic logic;
+	TicTacToeState state;
+	
+	public TicTacToeEngine(TicTacToeLogic logic, TicTacToeState state) {
+		this.logic = logic;
+		this.state = state;
+	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
